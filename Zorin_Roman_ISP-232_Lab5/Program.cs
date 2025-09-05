b40fc7c605bb8b70c08c39814aab6cb0103643bb
Роман Зорин <sgena170315@gmail.com>
2025-09-05 17:51:31 +0300

    Был добавлен пример вывода фраз Привет, *имя* на трёх языках с использованием методов

--- Zorin_Roman_ISP-232_Lab5/Program.cs
+++ Zorin_Roman_ISP-232_Lab5/Program.cs
@@ -11,9 +11,28 @@
             string myGame = "ARK: Survival Evolved";
             Console.WriteLine(myGame);
         }
+
         static void Main()
         {
             Game();
+
+            Console.WriteLine("Введите своё имя: ");
+            string? name = Console.ReadLine();
+            Console.Write("Введите ваш язык(русский, английский, немецкий): ");
+            string language = Console.ReadLine()!.ToLower();
+
+            void SayHelloRu() => Console.WriteLine($"Привет, {name}!");
+            void SayHelloEn() => Console.WriteLine($"Hello, {name}!");
+            void SayHelloDe() => Console.WriteLine($"Guten Tag, {name}!");
+
+            switch (language)
+            {
+                case "английский": SayHelloEn(); break;
+                case "русский": SayHelloRu(); break;
+                case "немецкий": SayHelloDe(); break;
+                default: Console.WriteLine("Язык не найден!"); break;
+            }
+
         }
     }
 }
