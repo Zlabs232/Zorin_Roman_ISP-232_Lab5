85c931a892edff118c0e019baeabfff0ef909ea7
Роман Зорин <sgena170315@gmail.com>
2025-09-05 17:38:26 +0300

    Add project files.

---
+++ Zorin_Roman_ISP-232_Lab5/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab4
+{
+    class Lab4
+    {
+        void SayHello()
+        {
+            Console.WriteLine("Hello"); //void SayHello() => Console.WriteLine("Hello");
+        }
+        static void Main()
+        {
+
+        }
+    }
+}
